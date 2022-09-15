b95848404b601463018a8fed3312cd6120619899
tjs6595 <t.schultejans@schenckprocess.com>
2022-09-14 19:12:26 -0500

    IAM-5-Authorization-In-Rest-Rant-Sessions - Activity - Complete.

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -23,19 +23,11 @@ router.post('/', async (req, res) => {
 }) 
 
 
+  
 router.get('/profile', async (req, res) => {
-    console.log(req.session.userId)
-    try {
-        let user = await User.findOne({
-            where: {
-                userId: req.session.userId
-            }
-        })
-        res.json(user)
-    } catch {
-        res.json(null)
-    }
+    res.json(req.currentUser)
 })
+
 
 
 module.exports = router
